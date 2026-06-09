60802975425830f73f4b70138925ec6d6c22ea80
Modiholodri <reinhold.lauer@gmail.com>
2026-06-09 20:44:34 +0700

    Create Move class

---
+++ src/Move.js
@@ -0,0 +1,73 @@
+class Move {
+  constructor(rank, move, winningChances, losingChances, equity, lostEquity) {
+    this.rank = rank;
+    this.move = move;
+    this.winningChances = winningChances;
+    this.losingChances = losingChances;
+    this.equity = equity;
+    this.lostEquity = lostEquity;
+  }
+
+  getRank() {
+    return this.rank;
+  }
+
+  setRank(rank) {
+    this.rank = rank;
+  }
+
+  getMove() {
+    return this.move;
+  }
+
+  setMove(move) {
+    this.move = move;
+  }
+
+  getWinningChances() {
+    return this.winningChances;
+  }
+
+  setWinningChances(winningChances) {
+    this.winningChances = winningChances;
+  }
+
+  getLosingChances() {
+    return this.losingChances;
+  }
+
+  setLosingChances(losingChances) {
+    this.losingChances = losingChances;
+  }
+
+  getEquity() {
+    return this.equity;
+  }
+
+  setEquity(equity) {
+    this.equity = equity;
+  }
+
+  getLostEquity() {
+    return this.lostEquity;
+  }
+
+  setLostEquity(lostEquity) {
+    this.lostEquity = lostEquity;
+  }
+
+  toString() {
+    return `Rank: ${this.rank}, Move: ${this.move}, Win%: ${this.winningChances}, Loss%: ${this.losingChances}, Equity: ${this.equity}, Lost Equity: ${this.lostEquity}`;
+  }
+
+  equals(other) {
+    return this instanceof Move &&
+           other instanceof Move &&
+           this.rank === other.rank &&
+           this.move === other.move &&
+           this.winningChances === other.winningChances &&
+           this.losingChances === other.losingChances &&
+           this.equity === other.equity &&
+           this.lostEquity === other.lostEquity;
+  }
+}
